9985b4cd86dfb11dd9007ccc8e419d00bcba4b97
Guitao Liu <guitao.liu@Outlook.com>
2020-10-20 17:41:19 +0800

    fix: fix bug in turning

--- plugins/motor.py
+++ plugins/motor.py
@@ -8,6 +8,8 @@ from config import Config
 class Motor:
 
     def __init__(self):
+        """Motor is for controlling the movement of the car.
+        """
         GPIO.setup(Config.MOTOR_1_GPIO_BCM, GPIO.OUT)
         GPIO.setup(Config.MOTOR_2_GPIO_BCM, GPIO.OUT)
         GPIO.setup(Config.MOTOR_3_GPIO_BCM, GPIO.OUT)
@@ -21,45 +23,77 @@ class Motor:
         self.initital()
 
     def initital(self) -> None:
+        """Initialized the motor
+        """
         self.motor_1.start(0)
         self.motor_2.start(0)
         self.motor_3.start(0)
         self.motor_4.start(0)
 
     def stop(self) -> None:
+        """Stop the motor engine. After this process, you need to call Motor.initital to initialize the motor.
+        """
         self.motor_1.stop()
         self.motor_2.stop()
         self.motor_3.stop()
         self.motor_4.stop()
 
     def forward(self, speed: int) -> None:
+        """Move forward
+
+        Args:
+            speed (int): motor electrical signal duty cycle, from 0 to 100.
+        """
         self.motor_1.ChangeDutyCycle(speed)
         self.motor_2.ChangeDutyCycle(0)
         self.motor_3.ChangeDutyCycle(speed)
         self.motor_4.ChangeDutyCycle(0)
 
     def backword(self, speed: int) -> None:
+        """Move backword
+
+        Args:
+            speed (int): motor electrical signal duty cycle, from 0 to 100.
+        """
         self.motor_1.ChangeDutyCycle(0)
         self.motor_2.ChangeDutyCycle(speed)
         self.motor_3.ChangeDutyCycle(0)
         self.motor_4.ChangeDutyCycle(speed)
 
     def pause(self) -> None:
+        """Stop the car
+        """
         self.initital()
 
-    def turn_in_moving(self, speed_left: int, speed_right: int) -> None:
-        self.motor_1.ChangeDutyCycle(speed_left)
+    def turn_in_moving(self, speed_right: int, speed_left: int) -> None:
+        """Seems cannot work.
+
+        Args:
+            speed_right (int): motor electrical signal duty cycle for wheels in the left side, from 0 to 100.
+            speed_left (int): motor electrical signal duty cycle for wheels in the right side, from 0 to 100.
+        """
+        self.motor_1.ChangeDutyCycle(speed_right)
         self.motor_2.ChangeDutyCycle(0)
-        self.motor_3.ChangeDutyCycle(speed_right)
+        self.motor_3.ChangeDutyCycle(speed_left)
         self.motor_4.ChangeDutyCycle(0)
 
-    def turn_left_in_place(self, speed: int) -> None:
+    def turn_right_in_place(self, speed: int) -> None:
+        """Turn left with the left back wheel as the origin.
+
+        Args:
+            speed (int): motor electrical signal duty cycle。
+        """
         self.motor_1.ChangeDutyCycle(0)
         self.motor_2.ChangeDutyCycle(speed)
         self.motor_3.ChangeDutyCycle(speed)
         self.motor_4.ChangeDutyCycle(0)
 
-    def turn_right_in_place(self, speed: int) -> None:
+    def turn_left_in_place(self, speed: int) -> None:
+        """Turn right with the right back wheel as the origin.
+
+        Args:
+            speed (int): motor electrical signal duty cycle。
+        """
         self.motor_1.ChangeDutyCycle(speed)
         self.motor_2.ChangeDutyCycle(0)
         self.motor_3.ChangeDutyCycle(0)
@@ -72,13 +106,11 @@ def test_motor():
     try:
         motor.forward(50)
         time.sleep(2)
-        motor.backword(50)
-        time.sleep(2)
-        motor.turn_left_in_place(50)
-        time.sleep(2)
-        motor.turn_right_in_place(50)
-        time.sleep(2)
-        motor.turn_in_moving(30, 50)
+        # motor.backword(50)
+        # time.sleep(2)
+        motor.turn_in_moving(50, 10)
+        time.sleep(0.5)
+        motor.forward(50)
         time.sleep(2)
         while 1:
             pass
