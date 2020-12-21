40c60fee62f8dd2da7074423f4d9d55a198aa607
Guitao Liu <guitao.liu@Outlook.com>
2020-12-21 17:38:19 +0800

    feat: add context manager for camera

--- sensors/camera.py
+++ sensors/camera.py
@@ -1,3 +1,4 @@
+import logging
 import threading
 import time
 
@@ -6,8 +7,16 @@ import numpy as np
 import picamera
 from picamera.array import PiRGBArray
 
+logger = logging.getLogger(__name__)
+
 
 class Camera:
+    """Camera provide entry for get camera frames with formats of BGR numpy ndarray and raw jepg.
+
+    Please use Camera with context manager to avoid not closing the camera rightly.
+
+    """
+
     def __init__(self) -> None:
         self.ca = picamera.PiCamera()
         self.ca.resolution = (640, 480)
@@ -16,17 +25,40 @@ class Camera:
         self.array = PiRGBArray(self.ca, size=(640, 480))
         self.array_np = None
         self.frame = None
+
+    def __enter__(self):
+        logger.debug("Camera started.")
+        self.start()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        logger.debug("Camera closed.")
+        self.ca.close()
+
+    def start(self) -> None:
         self.ca.start_preview()
 
         time.sleep(1)
-        threading.Thread(target=lambda: self.gen()).start()
+        print("Process started")
+        threading.Thread(target=lambda: self._gen()).start()
+        time.sleep(1)
 
-    def gen(self):
+    def close(self):
+        self.ca.close()
+
+    def _gen(self):
+        """
+        Generate raw numpy array and jpeg raw data. The numpy array is stored in self.array_np, and the jpeg data is
+        stored in self.frame.
+        """
         for r in self.ca.capture_continuous(self.array, "bgr", use_video_port=True):
             self.array_np = np.copy(r.array)
-            self.jpeg()
-            self.array.truncate(size=0)
+            self._jpeg()
+            self.array.truncate(0)
 
-    def jpeg(self):
+    def _jpeg(self):
+        """
+        Convert raw numpy array to jpeg data.
+        """
         _, buf = cv2.imencode(".jpeg", self.array_np)
         self.frame = buf
--- web/flasky.py
+++ web/flasky.py
@@ -11,12 +11,13 @@ def index():
     return render_template("index.html")
 
 
-def gen(ca: Camera):
-    while True:
-        frame = ca.frame.tostring()
-        yield b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n"
+def gen():
+    with camera as ca:
+        while True:
+            frame = ca.frame.tostring()
+            yield b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n"
 
 
 @app.route("/cameraStream")
 def camera_stream():
-    return Response(gen(camera), mimetype="multipart/x-mixed-replace; boundary=frame")
+    return Response(gen(), mimetype="multipart/x-mixed-replace; boundary=frame")
