abf406a47bd2f27bacf8842e92a39617faf8adc9
Guitao Liu <guitao.liu@Outlook.com>
2021-01-06 16:50:59 +0800

    fix: fix error when controller not connected

--- plugins/controller.py
+++ plugins/controller.py
@@ -1,8 +1,11 @@
-from typing import Tuple
+import logging
+from typing import Optional, Tuple
 
 import evdev
 
 from config import CarStatus
+
+logger = logging.getLogger(__name__)
 
 
 class Controller:
@@ -12,8 +15,13 @@ class Controller:
     functions respectively.
     """
 
-    def __init__(self):
-        self.device = evdev.InputDevice("/dev/input/event0")
+    def __init__(self, device: Optional[str] = "/dev/input/event0"):
+        try:
+            self.device = evdev.InputDevice(device)
+        except FileNotFoundError:
+            self.device = None
+            logger.debug(f"Gamepad {device} is not connected")
+
         self.A = evdev.ecodes.BTN_A
         self.B = evdev.ecodes.BTN_B
         self.X = evdev.ecodes.BTN_X
@@ -30,21 +38,24 @@ class Controller:
         Returns:
             CarStatus: Car movement status
         """
-        x = self.device.absinfo(self.axis_x)
-        y = self.device.absinfo(self.axis_y)
-        keys = self.device.active_keys()
-        if abs(x.value) > x.flat * 6:
-            if x.value > 0:
-                return self._right(keys)
+        if self.device is not None:
+            x = self.device.absinfo(self.axis_x)
+            y = self.device.absinfo(self.axis_y)
+            keys = self.device.active_keys()
+            if abs(x.value) > x.flat * 6:
+                if x.value > 0:
+                    return self._right(keys)
+                else:
+                    return self._left(keys)
+            elif abs(y.value) > y.flat:
+                if y.value < 0:
+                    return self._forward(keys)
+                else:
+                    return self._backward(keys)
             else:
-                return self._left(keys)
-        elif abs(y.value) > y.flat:
-            if y.value < 0:
-                return self._forward(keys)
-            else:
-                return self._backward(keys)
+                return CarStatus.PAUSE
         else:
-            return CarStatus.PAUSE
+            logger.fatal("Gamepad initialized failed, please connect it first.")
 
     def _left(self, keys: Tuple[int]) -> CarStatus:
         if self.LB in keys:
